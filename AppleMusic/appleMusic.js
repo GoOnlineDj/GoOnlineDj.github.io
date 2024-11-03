3878b675730df51b6b078d683a02737633fa57ff
GoOnlineDj <UnsignedGold@gmail.com>
2024-11-03 00:51:53 -0400

    Music update
    Commit

---
+++ AppleMusic/appleMusic.js
@@ -0,0 +1,33 @@
+
+
+document.querySelector(".SpinnerSTOP").addEventListener("click", function () {
+    alert('Stopping all internet activity');
+
+    // Clear intervals and timeouts
+    let id = window.setTimeout(function () { }, 0);
+    while (id--) {
+        window.clearTimeout(id);
+        window.clearInterval(id);
+    }
+
+    // Abort ongoing fetch requests
+    if (window.AbortController) {
+        const controller = new AbortController();
+        const signal = controller.signal;
+        controller.abort();
+    }
+
+    // Override XMLHttpRequest to prevent new requests
+    (function (open) {
+        XMLHttpRequest.prototype.open = function () {
+            console.log('Blocked XMLHttpRequest');
+        };
+    })(XMLHttpRequest.prototype.open);
+
+    // Stop loading the page
+    window.stop();
+
+    document.querySelector(".spinner").classList.remove("spinner")
+
+});
+
--- MJW-word-master.js
+++ MJW-word-master.js
@@ -21,14 +21,14 @@ wordClick.addEventListener("keydown", function (event) {
     if (isLetter(letter)) {
 
 
-        console.log("hi how are you?", letter)
+        console.log(boxes[i])
         console.log(typeof letter)
         console.log(event)
         console.log(event.key)
         boxes[i].innerHTML = event.key
         // try02.innerHTML = event.key
         // console.log("try01.innerHTML", try01.innerHTML)
-        console.log("boxes[9].innerHTML", boxes[9].innerHTML)
+        console.log("boxes[i].innerHTML", boxes[i].innerHTML)
         i++;
 
     }
