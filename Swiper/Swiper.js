82b93bbb4e68b1bfbddeb07c056c4bc8ca2da1c6
GirlMeetGuy <UnsignedGold@gmail.com>
2025-01-13 13:56:48 -0500

    update
    update

--- Swiper/Swiper.js
+++ Swiper/Swiper.js
@@ -1,12 +1,55 @@
 
-document.getElementById("_id_Good-News").innerHTML = setInterval(() => {
+setInterval(() => {
     document.getElementById("_id_Good-News").innerHTML = new Date().toLocaleTimeString();
-  }, 1000);
-
+}, 1000);
 
   
+    // Include Swiper library
+    // import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js';
+
+    const loadframe = document.getElementById("_id_frame");
+loadframe.innerHTML = `
+
+
+                  <div class="swiper-container">
+                   <div class="_c_space-ship"> 
+                        🛸
+                    </div>
+            <div class="swiper-wrapper">
+                        <div class="swiper-slide"> <img src="./images/17.jpg" alt="Michael Junior Williams"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-W3Schools/Michael_J_Williams_Certificate_of_Completion_Front_End_Development_Michael_Junior_Williams.png" alt="W3Schools"></div>
+                        <div class="swiper-slide"> <img src="./images/web-development-W3Schools/Michael_J_Williams_Certificate_of_Completion_JavaScript_Michael_Junior_Williams.png" alt="W3Schools"></div>
+                        <div class="swiper-slide"> <img src="./images/web-development-W3Schools/Michael_J_Williams_Certificate_of_Completion_CSS_Michael_Junior_Williams.png" alt="W3Schools"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-W3Schools/Michael_J_Williams_Certificate_of_Completion_HTML_Michael_Junior_Williams.png" alt="W3Schools"></div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Introduction-HTML.png" alt="Frontend Masters"></div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Introduction-CSS.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Introduction-HTML_Forms.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Introduction_Website_Embeds_GitHub_Pages.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Calculator_Project_HTML_CSS.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Introduction_to_JavaScript.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Calculator_Project_JavaScript.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Using_JavaScript_in_Websites.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Build_a_Game_Project_Feed-A-Star-Mole.png" alt="Frontend_Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Career_Guidance.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Complete_Intro_to_Web_Development_v3.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Getting_Started_with_CSS.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Getting_Started_with_JavaScript_v2.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_CSS_Foundations.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_JavaScript_From_First_Steps_to_Professional.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Website_Accessibility_v2.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Getting_a_Software_Engineering_Job_v3.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Web_Development_Project_Personal_Portfolio_Website.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Software_Developer_Success_Soft_Skills_Testing.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Introduction_to_Dev_Tools_v3.png" alt="Frontend Masters"> </div>
+                        <div class="swiper-slide"> <img src="./images/web-development-Frontend-Masters/Michael_Junior_Williams_Frontend_Masters_Complete_Intro_to_Linux_and_the_Command-Line.png" alt="Frontend Masters"> </div>
+
+                    </div>
+                    </div>
+                    
+`;
+
     // Swiper
-    new Swiper(".swiper", {
+    new Swiper(".swiper-container", {
         speed: 300,
         effect: "flip",
         loop: true,
@@ -16,7 +59,7 @@ document.getElementById("_id_Good-News").innerHTML = setInterval(() => {
             prevEl: ".swiper-button-prev",
         },
         autoplay: {
-            delay: 7000, // Auto flip every 7 seconds
+            delay: 1000, // Auto flip every 7 seconds
             disableOnInteraction: false,
         },
     });
@@ -48,8 +91,3 @@ document.getElementById("_id_Good-News").innerHTML = setInterval(() => {
         });
 
 
-
-
-
-
-       
